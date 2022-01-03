2a39470188ee9c0fa8c5732594e4489b6d3db3f3
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtech.io>
2022-01-03 11:27:26 +0700

    Initial commit

---
+++ Biometrics-login/Biometrics-login/Biometrics_loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometrics_loginApp.swift
+//  Biometrics-login
+//
+//  Created by Phonthep Aungkanukulwit on 3/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Biometrics_loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Biometrics-login/Biometrics-login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Biometrics-login
+//
+//  Created by Phonthep Aungkanukulwit on 3/1/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
